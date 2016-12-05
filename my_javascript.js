34bc9f23ba42a97503c10d67a073f0d1cf4b446d
chuckwalla <jeelthompson@gmail.com>
2016-12-05 08:07:12 -0800

    more jquery

--- my_javascript.js
+++ my_javascript.js
@@ -18,14 +18,31 @@ $(document).ready(function() {
  
     });
 
+    // not needed anymore since I have the doc.on click going now
     // fades list items inside #right on the page when they are clicked
-    $('#right li').click(function() {
-    	$(this).fadeOut('slow');
-    });
+    // $('#right li').click(function() {
+    // 	$(this).fadeOut('slow');
+    // });
 
     //brings back the #right list items when clicking #comeback
     $('#comeback').click(function(){
     	$('#right li').fadeIn('slow');
+    });
+
+    $('#addmore').click(function(){
+    	$('ul').append('<li>Another one!</li>');
+    });
+
+    $('#comebackfirst').click(function() {
+    	$('#comeback').after($('#addmore'));
+    });
+
+    $('#addmorefirst').click(function() {
+    	$('#addmore').after($('#comeback'));
+    });
+
+    $(document).on('click', '#right li', function() {
+    	$(this).fadeOut('slow');
     });
  
 });
